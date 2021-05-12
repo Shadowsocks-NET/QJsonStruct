b3eb30e89da5094fbd78497850d30c7f147a1785
database64128 <free122448@hotmail.com>
2021-05-12 15:35:13 +0800

    🔕 Fix MSVC warnings

--- QJsonStructBase.hpp
+++ QJsonStructBase.hpp
@@ -2,5 +2,10 @@
 
 #define JS_F(x) x
 
+#ifdef _MSC_VER
+#define JS_MACRO_ARGUMENT_NO_WARN
+#define JS_MACRO_ARGUMENT_RESTORE_WARN
+#else
 #define JS_MACRO_ARGUMENT_NO_WARN _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wgnu-zero-variadic-macro-arguments\"")
 #define JS_MACRO_ARGUMENT_RESTORE_WARN _Pragma("clang diagnostic pop")
+#endif
